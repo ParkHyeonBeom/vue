92c794b7a0a92cb10f56b0d7f2f8a089b14dab36
Hosae0905 <didghtls11@gmail.com>
2024-01-31 14:30:07 +0900

    [Feat] 경로 설정 변경

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import VerifyPage from '@/page/VerifyPage.vue'
 import LoginPage from '@/page/LoginPage.vue'
 import SignUpPage from "@/page/SignUpPage.vue";
+import CartPage from "@/page/CartPage.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes : [
         { path: '/verify', component: VerifyPage },
-        { path: '/login', component: LoginPage },
-        { path: '/signup', component: SignUpPage },
-
+        { path: '/member/login', component: LoginPage },
+        { path: '/member/signup', component: SignUpPage },
+        {path: '/cart', component: CartPage},
     ]
 })
 
